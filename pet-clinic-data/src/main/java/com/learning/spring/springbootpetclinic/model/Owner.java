ef15a498f9f12c93be962b5e6a52dd1ecfcf01bb
Umanath Jha <jhaumanath93@gmail.com>
2020-07-19 16:37:58 +0530

    Initial Multi-module project setup

---
+++ pet-clinic-data/src/main/java/com/learning/spring/springbootpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.learning.spring.springbootpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ pet-clinic-data/src/main/java/com/learning/spring/springbootpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.learning.spring.springbootpetclinic.model;
+
+public class Vet extends Person {
+}
